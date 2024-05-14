79bd9a07f441d773e879b278da93dd47e89818da
wearenotwhoiam <weareryanmeyers@gmail.com>
2024-05-13 18:46:26 -0700

    ENTIRE PROJECT

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
+filePath = "books/frankenstein.txt"
+
 def main():
     book = GetText()
-    print(book)
-    countWords(book)
+    print(f"---Beginning report on {filePath}---")
+    print(f"{CountWords(book)} words found in the book\n")
 
-def countWords(book):
+    Display(SortDictionary(CountLetters(book)))
+
+def CountWords(book):
     words = book.split()
-    print(len(words))
+    return len(words)
 
 def GetText():
-    with open("books/frankenstein.txt", "r") as book:
+    with open(filePath, "r") as book:
         bookText = book.read()
         return bookText
+    
+def CountLetters(bookText):
+    #return number of times each character appears
+    lower = bookText.lower()
+    letterCountDict = {}
+
+    for letter in lower:
+        letterCount = 0
+        if letter in letterCountDict:
+            letterCount = letterCountDict[letter]
+        letterCountDict[letter] = letterCount + 1
+    return letterCountDict
+
+def SortOn(list):
+    return list["count"]
+
+def SortDictionary(dict):
+    sorted = []
+    for letter in dict:
+        if letter.isalpha():
+            letter_dict = {"letter": letter, "count": dict[letter]}
+            sorted.append(letter_dict)
+    sorted.sort(reverse=True, key=SortOn)
+    return sorted
+
+def Display(sorted):
+    for x in sorted:
+        print(f"The '{x["letter"]}' character was printed {x["count"]} times")
+
 main()
